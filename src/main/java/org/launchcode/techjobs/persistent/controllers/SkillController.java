6a275c511858f4c4e767ff3349cb74a8d6d1c42f
Jasmine Lim <jasmine_lim13@yahoo.com>
2023-09-13 15:32:20 -0500

    employer controller

---
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
@@ -0,0 +1,15 @@
+package org.launchcode.techjobs.persistent.controllers;
+
+import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+public class SkillController {
+
+}
