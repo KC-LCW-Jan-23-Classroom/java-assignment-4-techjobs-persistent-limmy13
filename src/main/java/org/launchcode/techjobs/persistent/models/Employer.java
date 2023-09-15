16ee69fabcd8761b112308d18243f691a87afab2
Jasmine Lim <jasmine_lim13@yahoo.com>
2023-09-14 21:54:55 -0500

    task 3

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,12 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -11,10 +15,15 @@ public class Employer extends AbstractEntity {
     @Size(max = 150)
     private String location;
 
+    @OneToMany(mappedBy = "employer")
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
     // no arg constructor
     public Employer(){
 
     }
+
 
     public Employer(String location) {
         this.location = location;
